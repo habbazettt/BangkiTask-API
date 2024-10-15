960d68342bab8b7e6625f829a07efacf527442f2
habbaza <habbazahubbal@gmail.com>
2024-10-15 12:40:23 +0700

    Edit cors configuration

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ const { authenticateToken } = require('./utilities')
 app.use(express.json())
 app.use(cors({
     origin: '*',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }))
 
 app.get('/', (req, res) => {
