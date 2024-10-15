4194eda3e5fbf5a42ce1581deb1e391767afd6b5
habbaza <habbazahubbal@gmail.com>
2024-10-15 14:14:34 +0700

    edit cors config

--- index.js
+++ index.js
@@ -18,11 +18,16 @@ const { authenticateToken } = require('./utilities')
 
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://bangki-task-app.vercel.app'],
+    origin: '*',
     credentials: true,
 }));
 
 app.get('/', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
     res.send('Hello World!')
 })
 
